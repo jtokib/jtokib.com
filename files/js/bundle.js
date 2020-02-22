86c48840250eba66037ed43092cc4fbe4e0e4a2f
jtokib <jtokib@hotmail.com>
2020-02-21 20:32:11 -0800

    Add SW Back

--- files/js/bundle.js
+++ files/js/bundle.js
@@ -149,14 +149,14 @@
 
 })();
 
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', () => {
-//         navigator.serviceWorker.register('sw.js', {
-//             updateViaCache: 'all'
-//         }).then((registration) => {
-//             console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//         }, (err) => {
-//             console.log('ServiceWorker registration failed: ', err);
-//         });
-//     });
-// }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('sw.js', {
+            updateViaCache: 'all'
+        }).then((registration) => {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, (err) => {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
